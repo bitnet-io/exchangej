37136b06eb17072edfc0e32565404c42ed564c67
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2023-03-06 03:36:41 -0700

    Update LegacyAddress.java

--- core/src/main/java/org/bitcoinj/core/LegacyAddress.java
+++ core/src/main/java/org/bitcoinj/core/LegacyAddress.java
@@ -160,9 +160,10 @@ public class LegacyAddress extends Address {
         } else {
             if (version == params.getAddressHeader())
                 return new LegacyAddress(params, false, bytes);
-           // else if (version == params.getP2SHHeader())
-             //   return new LegacyAddress(params, true, bytes);
-            // throw new AddressFormatException.WrongNetwork(version);
+            else if (version == params.getP2SHHeader())
+                return new LegacyAddress(params, true, bytes);
+            }
+           // throw new AddressFormatException.WrongNetwork(version);
         }
     }
 
