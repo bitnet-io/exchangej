43e651698523faaa34f52c6bfdbc4bbe5dff3476
root <root@nwstrtrj01.rd.lv.cox.cci>
2023-03-05 19:53:05 -0700

    push for mainnet

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -38,27 +38,33 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         targetTimespan = TARGET_TIMESPAN;
         maxTarget = Utils.decodeCompactBits(0x1e0ffff0L);
         dumpedPrivateKeyHeader = 158;
-        addressHeader = 60;
+        addressHeader = 25;
         p2shHeader = 22;
-        segwitAddressHrp = "radc";
+        segwitAddressHrp = "bit";
         port = 8333;
-        packetMagic = 0xd1d1d1d1L;
-        bip32HeaderP2PKHpub = 0x02facafd; //The 4 byte header that serializes in base58 to "dgub".
-        bip32HeaderP2PKHpriv =  0x02fac398; 
+        packetMagic = 0xd1e1d1e1L;
+        //bip32HeaderP2PKHpub = 0x02facafd; //The 4 byte header that serializes in base58 to "dgub".
+        //bip32HeaderP2PKHpriv =  0x02fac398; 
+
+        bip32HeaderP2PKHpub = 0x0488b21e; // The 4 byte header that serializes in base58 to "xpub".
+        bip32HeaderP2PKHpriv = 0x0488ade4; // The 4 byte header that serializes in base58 to "xprv"
+        bip32HeaderP2WPKHpub = 0x04b24746; // The 4 byte header that serializes in base58 to "zpub".
+        bip32HeaderP2WPKHpriv = 0x04b2430c; // The 4 byte header that serializes in base58 to "zprv"
+
 
         majorityEnforceBlockUpgrade = MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE;
         majorityRejectBlockOutdated = MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED;
         majorityWindow = MAINNET_MAJORITY_WINDOW;
 
  genesisBlock.setDifficultyTarget(0x1e0ffff0L);
-        genesisBlock.setTime(1622171724L);
-        genesisBlock.setNonce(491158L);
+        genesisBlock.setTime(1677414786L);
+        genesisBlock.setNonce(1196422L);
         subsidyDecreaseBlockCount = 100000;
         spendableCoinbaseDepth = 10;
 
         id = ID_MAINNET;
         String genesisHash = genesisBlock.getHashAsString();
-        checkState(genesisHash.equals("000007ce46e6c59844c34fa7ba5b27c8dac0653a27fcfb7340cc0158849e4afd"),
+        checkState(genesisHash.equals("00000caff19214512f927af2b7cead4b33a54c096e432a275628cb4cf8d4b73c"),
                 genesisHash);
 
         majorityEnforceBlockUpgrade = MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE;
@@ -69,14 +75,16 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
         // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handling.
         // Having these here simplifies block connection logic considerably.
-        checkpoints.put(60000, Sha256Hash.wrap("cf23fe03d921012cfdfc2847bd6ef4a647bbb559d6eb57d6432d7bcb930d720f"));
+//        checkpoints.put(60000, Sha256Hash.wrap("cf23fe03d921012cfdfc2847bd6ef4a647bbb559d6eb57d6432d7bcb930d720f"));
 
         
         
   // ?? risky business readding dogecoin dns seeds here to attempt experimentation with peer to peer "bloom filter bit" that might be missing from radiocoin dns seed points for android wallet support (in experimentation to match protocol header)
         dnsSeeds = new String[] {
-                "radiopool.me",
-            "46.101.169.238",
+                "bitexplorer.io",
+            "seed.bitexplorer.io",
+            "us.mining4people.com",
+            "fi.mining4people.com",
             };
     }
     private static MainNetParams instance;
