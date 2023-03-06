a1aefa7e8815ba7761ec0fcd5219b0ecae8d648b
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2023-03-06 03:40:28 -0700

    Update LegacyAddress.java

--- core/src/main/java/org/bitcoinj/core/LegacyAddress.java
+++ core/src/main/java/org/bitcoinj/core/LegacyAddress.java
@@ -145,7 +145,7 @@ public class LegacyAddress extends Address {
      */
    
    public static LegacyAddress fromBase58(@Nullable NetworkParameters params, String base58) 
-       throws AddressFormatException, AddressFormatException.WrongNetwork {
+       //throws AddressFormatException, AddressFormatException.WrongNetwork {
         byte[] versionAndDataBytes = Base58.decodeChecked(base58);
         int version = versionAndDataBytes[0] & 0xFF;
         byte[] bytes = Arrays.copyOfRange(versionAndDataBytes, 1, versionAndDataBytes.length);
@@ -156,14 +156,14 @@ public class LegacyAddress extends Address {
                 else if (version == p.getP2SHHeader())
                     return new LegacyAddress(p, true, bytes);
             }
-            throw new AddressFormatException.InvalidPrefix("No network found for " + base58);
+            //throw new AddressFormatException.InvalidPrefix("No network found for " + base58);
         } else {
             if (version == params.getAddressHeader())
                 return new LegacyAddress(params, false, bytes);
             else if (version == params.getP2SHHeader())
                 return new LegacyAddress(params, true, bytes);
-            else if (1 < 0)
-            throw new AddressFormatException.WrongNetwork(version);
+            //else if (1 < 0)
+            //throw new AddressFormatException.WrongNetwork(version);
         }
     }
 
