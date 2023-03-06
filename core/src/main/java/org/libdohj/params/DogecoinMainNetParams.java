14bbe11f2890f8164da065b8eb148e92caefc142
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2023-03-05 19:14:16 -0700

    Update DogecoinMainNetParams.java

--- core/src/main/java/org/libdohj/params/DogecoinMainNetParams.java
+++ core/src/main/java/org/libdohj/params/DogecoinMainNetParams.java
@@ -37,11 +37,11 @@ public class DogecoinMainNetParams extends AbstractDogecoinParams {
     public static final int MAINNET_MAJORITY_WINDOW = 2000;
     public static final int MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED = 1900;
     public static final int MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE = 1500;
-    private static final long GENESIS_TIME = 1622171724;
-    private static final long GENESIS_NONCE = 491158;
-    private static final Sha256Hash GENESIS_HASH = Sha256Hash.wrap("000007ce46e6c59844c34fa7ba5b27c8dac0653a27fcfb7340cc0158849e4afd");
+    private static final long GENESIS_TIME = 1677414786;
+    private static final long GENESIS_NONCE = 1196422;
+    private static final Sha256Hash GENESIS_HASH = Sha256Hash.wrap("00000caff19214512f927af2b7cead4b33a54c096e432a275628cb4cf8d4b73c");
 
-    public static final long STANDARD_MAX_DIFFICULTY_TARGET = 0x1e0ffff0L;
+    public static final long STANDARD_MAX_DIFFICULTY_TARGET = 0x1d00ffffL;
 
     protected static final int DIFFICULTY_CHANGE_TARGET = 100000;
 
@@ -55,26 +55,29 @@ public class DogecoinMainNetParams extends AbstractDogecoinParams {
 
         // not in this case would be 188 ... since 60 + 128 = 188 .......in this case its my own weird thing that still works as 30 + 128 for 158 from the standard desktop qt wallet from dogecoin actually using the proper value of address_prefix + 128(still works for radiocoin-qt as 158 as a customized value to decode the private key from address_prefix 60 "R")
         dumpedPrivateKeyHeader = 158; //This is always addressHeader + 128
-        addressHeader = 60;
+        addressHeader = 25;
         p2shHeader = 22;
         port = 8333;
-        packetMagic = 0xd1d1d1d1;
-        segwitAddressHrp = "radc";
+        packetMagic = 0xd1e1d1e1;
+        segwitAddressHrp = "bit";
         // Note that while BIP44 makes HD wallets chain-agnostic, for legacy
         // reasons we use a Doge-specific header for main net. At some point
         // we'll add independent headers for BIP32 legacy and BIP44.
-        bip32HeaderP2PKHpub = 0x02facafd; //The 4 byte header that serializes in base58 to "dgub".
-        bip32HeaderP2PKHpriv =  0x02fac398; //The 4 byte header that serializes in base58 to "dgpv".
-        genesisBlock.setDifficultyTarget(0x1e0ffff0L);
-        genesisBlock.setTime(1622171724L);
-        genesisBlock.setNonce(491158L);
+         bip32HeaderP2PKHpub = 0x0488b21e; // The 4 byte header that serializes in base58 to "xpub".
+        bip32HeaderP2PKHpriv = 0x0488ade4; // The 4 byte header that serializes in base58 to "xprv"
+        bip32HeaderP2WPKHpub = 0x04b24746; // The 4 byte header that serializes in base58 to "zpub".
+        bip32HeaderP2WPKHpriv = 0x04b2430c; // The 4 byte header that serializes in base58 to "zprv"
+        
+        genesisBlock.setDifficultyTarget(0x1d00ffffL);
+        genesisBlock.setTime(1677414786L);
+        genesisBlock.setNonce(1196422L);
         subsidyDecreaseBlockCount = 100000;
         spendableCoinbaseDepth = 10;
 
         // Note this is an SHA256 hash, not a Scrypt hash. Scrypt hashes are only
         // used in difficulty calculations.
         String genesisHash = genesisBlock.getHashAsString();
-        checkState(genesisHash.equals("000007ce46e6c59844c34fa7ba5b27c8dac0653a27fcfb7340cc0158849e4afd"),
+        checkState(genesisHash.equals("00000caff19214512f927af2b7cead4b33a54c096e432a275628cb4cf8d4b73c"),
                 genesisHash);
 
         majorityEnforceBlockUpgrade = MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE;
@@ -85,14 +88,16 @@ public class DogecoinMainNetParams extends AbstractDogecoinParams {
         // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
         // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handling.
         // Having these here simplifies block connection logic considerably.
-        checkpoints.put(60000, Sha256Hash.wrap("cf23fe03d921012cfdfc2847bd6ef4a647bbb559d6eb57d6432d7bcb930d720f"));
+        checkpoints.put(0, Sha256Hash.wrap("00000caff19214512f927af2b7cead4b33a54c096e432a275628cb4cf8d4b73c"));
 
         
         
   // ?? risky business readding dogecoin dns seeds here to attempt experimentation with peer to peer "bloom filter bit" that might be missing from radiocoin dns seed points for android wallet support (in experimentation to match protocol header)
         dnsSeeds = new String[] {
-                "radiopool.me",
-            "46.101.169.238",
+                "seed.bitexplorer.io",
+            "bitexplorer.io",
+            "us.mining4people.com",
+            "fi.mining4people.com",
             };
     }
     private static DogecoinMainNetParams instance;
